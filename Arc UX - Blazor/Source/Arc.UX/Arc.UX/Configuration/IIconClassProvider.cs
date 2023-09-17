1b1f6e2dd302d5955f76d8f831b0c4b6806eb437
JeffJacka <Jeff.Jacka@archy.tech>
2023-09-16 19:30:09 -0500

    Adding initial project and configuration

---
+++ Arc UX - Blazor/Source/Arc.UX/Arc.UX/Configuration/IIconClassProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Arc.UX.Configuration;
+
+public interface IIconStyleProvider
+{
+
+    string Icon(string? iconName, string? iconDesign, string? iconAnimation);
+}
---
+++ Arc UX - Blazor/Source/Arc.UX/Arc.UX/Configuration/IStyleProvider.cs
@@ -0,0 +1,11 @@
+﻿using Arc.UX.Components;
+
+namespace Arc.UX.Configuration;
+
+public interface IComponentStyleProvider
+{
+    public string Alert(string style);
+    public string Alert(ElementStyle style);
+
+    public string Text(ElementStyle style);
+}
