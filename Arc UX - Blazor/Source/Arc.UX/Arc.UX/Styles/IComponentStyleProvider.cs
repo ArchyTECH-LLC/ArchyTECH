fcbd7ab44cb43d9e8988568530bdfd2c20019788
JeffJacka <Jeff.Jacka@archy.tech>
2023-09-18 10:17:29 -0500

    Renaming components with Arc prefix

--- Arc UX - Blazor/Source/Arc.UX/Arc.UX/Styles/IComponentStyleProvider.cs
+++ Arc UX - Blazor/Source/Arc.UX/Arc.UX/Styles/IComponentStyleProvider.cs
@@ -4,9 +4,9 @@ namespace Arc.UX.Styles;
 
 public interface IComponentStyleProvider
 {
-    public string Alert(string style);
-    public string Alert(ElementStyle style);
+    public string Alert(string? style);
+    public string Alert(ElementStyle? style);
 
-    public string Text(ElementStyle style);
-    public string Style(ElementStyle style);
+    public string Text(ElementStyle? style);
+    public string Style(ElementStyle? style);
 }
